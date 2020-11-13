b737cb867f3e33a23c3eb795d521ad5a72c992f2
ChiragSehra <chiragsehra42@gmail.com>
2020-11-14 00:22:36 +0530

    Adding YAMLHandler

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,6 +2,8 @@ package urlshort
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler function
@@ -16,4 +18,27 @@ func MapHandler(pathToUrls map[string]string, fallback http.Handler) http.Handle
 		//else
 		fallback.ServeHTTP(w, r)
 	}
+}
+
+// YAMLHandler function
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	//1. Parse the yaml somehow
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(yamlBytes, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	//2. Convert YAML to map
+	pathToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathToUrls[pu.Path] = pu.URL
+	}
+	//3. Return a mapHandler using the map
+	return MapHandler(pathToUrls, fallback), nil
+
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,33 +1,44 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/gophercises/urlshort"
+	"fmt"
+	"net/http"
+
+	"github.com/gophercises/urlshort"
 )
 
-func main(){
-  mux := defaultMux()
+func main() {
+	mux := defaultMux()
 
-  // Build the MapHandler using the mux as a fallback
-  pathToUrls := map[string]string{
-    "url/short-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-    "/yaml-godoc": "https://godoc.org/gopkg.in/yaml.v2",
-  }
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-  mapHandler := urlshort.MapHandler(pathToUrls, mux)
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+	- path: /urlshort
+	  url: https://github.com/gophercises/urlshort
+	- path: /urlshort-final
+	  url: https://github.com/gophercises/urlshort/tree/solution
+	`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
 
-  // Build the YAMLHandler using the MapHandler as the fallback
-  yaml := `
-  -path: \urlshort
-  url: https://github.com/gophercises/urlshort
-  -path: \urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/final
-  `
-  yamlHandler, err := urlshort.YamlHandler([]byte(yaml), mapHandler)
-  if err!= nil {
-    panic(err)
-  }
-  fmt.Println("Starting the server on: 8080")
-  http.ListenAndServe("":8000", yamlHandler)
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
